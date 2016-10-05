5d11e7b9d8e10381a70718e8ac26b82482d70e16
Stefan <stefan.kaalen@hotmail.com>
2016-10-05 18:41:05 +0200

    Börjat med controller.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private Button clickMeBtn;
+    @FXML
+    private TextField textField;
+
+    public void clickMeBtnPressed(){
+        System.out.println(textField.getText());
+
+    }
+
 }
