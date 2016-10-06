07ff028479add00536ce6abd4ea53c48786b9177
christianekenstedt <christian>
2016-10-06 16:59:24 +0200

    Question importer read file and creates questions from the file.

--- src/sample/Controller/Controller.java
+++ src/sample/Controller/Controller.java
@@ -3,6 +3,11 @@ package sample.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import sample.Model.Question;
+import sample.Model.QuestionImporter;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Controller {
     @FXML
@@ -32,6 +37,16 @@ public class Controller {
     @FXML
     private MenuItem miImportQuestions;
 
+    private ArrayList<Question> questions;
+    @FXML
+    void miImportQuestionsClicked(ActionEvent event){
+        try {
+            questions = QuestionImporter.getQuestionsFrom("src/questions.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @FXML
     void backBtnPressed(ActionEvent event) {
 
@@ -39,6 +54,15 @@ public class Controller {
 
     @FXML
     void nextBtnPressed(ActionEvent event) {
+        System.out.println("Questions read from file: ");
+        for (Question q: questions){
+            System.out.println(q.getDescription() + " with choices: " +q.getChoices().toString());
+        }
+        questionDescription.setText(questions.get(0).getDescription());
+        radioOne.setText(questions.get(0).getChoices().get(0));
+        radioTwo.setText(questions.get(0).getChoices().get(1));
+        radioThree.setText(questions.get(0).getChoices().get(2));
+        radioFour.setText(questions.get(0).getChoices().get(3));
 
     }
 }
--- src/sample/Model/QuestionImporter.java
+++ src/sample/Model/QuestionImporter.java
@@ -1,8 +1,58 @@
 package sample.Model;
 
+import java.io.*;
+import java.util.ArrayList;
+
+import static java.lang.System.in;
+
 /**
  * Created by chris on 2016-10-06.
  */
 public class QuestionImporter {
+
+
+    public static ArrayList<Question> getQuestionsFrom(String filepath) throws IOException {
+        ArrayList<Question> questions = new ArrayList<>();
+        BufferedReader br = null;
+        try{
+            br = new BufferedReader(new FileReader(filepath));
+            String line = br.readLine();
+            while (line != null){
+                Question q = questionBuilder(line);
+                if (q != null) questions.add(q);
+                line = br.readLine();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally{
+            if(br != null)
+                br.close();
+        }
+        return questions;
+    }
+
+    private static Question questionBuilder(String line){
+        if (line.startsWith("%")) return null;
+
+        int point = -1;
+        String desc = "";
+        String correctAnswer = "";
+        ArrayList<String> choices = new ArrayList<>();
+
+        String[] splits = line.split("::");
+
+        desc = splits[0];
+        point = Integer.valueOf(splits[1]);
+        correctAnswer = splits[2];
+        String[] ch = splits[3].split(",,");
+
+        for (int i = 0; i < ch.length; i++){
+            choices.add(ch[i]);
+        }
+
+        return new Question(point,desc,choices,correctAnswer);
+    }
 
 }
