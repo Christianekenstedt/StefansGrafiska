24f3299779134cc1ca58c1020797341b5b3f6d98
Stefan <stefan.kaalen@hotmail.com>
2016-10-06 16:16:57 +0200

    Börjat med controller.

--- src/sample/Model/Question.java
+++ src/sample/Model/Question.java
@@ -11,4 +11,42 @@ public class Question {
     private ArrayList<String> choices;
     private String correctAnswer;
 
+    public Question(int point, String description, ArrayList<String> choices, String correctAnswer) {
+        this.point = point;
+        this.description = description;
+        this.choices = choices;
+        this.correctAnswer = correctAnswer;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+
+    public void setChoices(ArrayList<String> choices) {
+        this.choices = choices;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
 }
